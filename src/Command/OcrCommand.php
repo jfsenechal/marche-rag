a6d1b8b06aaa9e299c3cdb8e1bf6fd97e4b8bb7d
jfsenechal <jfsenechal@gmail.com>
2025-11-12 16:02:17 +0100

    add ocr

---
+++ src/Command/OcrCommand.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Command;
+
+use App\Ocr\Ocr;
+use App\Repository\MarcheBeRepository;
+use App\Repository\Theme;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:ocr',
+    description: 'Extract text from pdf files',
+)]
+class OcrCommand extends Command
+{
+    private SymfonyStyle $io;
+
+    public function __construct(
+        private readonly MarcheBeRepository $marcheBeRepository,
+        private readonly Ocr $ocr,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument('filePath', InputArgument::REQUIRED, 'The file path');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->io = new SymfonyStyle($input, $output);
+
+        $filePath = (string)$input->getArgument('filePath');
+
+        foreach (Theme::getSites() as $siteName) {
+            foreach ($this->marcheBeRepository->getAttachments($siteName) as $attachment) {
+                $this->io->writeln('Extracting pdf: '.$attachment->guid->rendered);
+                $filePath = $this->ocr->getAbsolutePathFromAttachment($attachment);
+                if ($filePath) {
+                    $this->io->writeln($filePath);
+                } else {
+                    dd($filePath);
+                }
+                if ($this->ocr->fileExists($filePath)) {
+                    $this->io->info('convert ');
+                    try {
+                        $this->ocr->convertToImages($filePath);
+                        $this->ocr->convertToTxt($filePath);
+                    } catch (\Exception$e) {
+                        $this->io->error($e->getMessage());
+                    }
+                    $this->io->success('Finished');
+                }
+            }
+        }
+
+        $this->io->success('Finished');
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Ocr/Ocr.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Ocr;
+
+use App\Repository\Theme;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Path;
+
+class Ocr
+{
+    public Filesystem $filesystem;
+    public static $ocrFilename = 'ocr.txt';
+    public string $department;
+
+    public function __construct(
+        #[Autowire('%kernel.project_dir%')] private readonly string $projectDir,
+        #[Autowire(env: 'WP_DIRECTORY')] private readonly string $wpDir,
+        #[Autowire(env: 'TMP_DIRECTORY')] private readonly string $tmpDir,
+    ) {
+        $this->filesystem = new Filesystem();
+    }
+
+    public function convertToImages(string $filePath): void
+    {
+        $tmpDirectory = $this->getTemporaryDirectory($filePath);
+        // Create the directory if it doesn't exist
+        $this->filesystem->mkdir($tmpDirectory);
+
+        shell_exec("pdftoppm -png \"$filePath\" $tmpDirectory/img-ocr");
+    }
+
+    public function getTemporaryDirectory(string $filePath): string
+    {
+        return dirname(str_replace($this->wpDir, $this->tmpDir, $filePath));
+    }
+
+    public function convertToTxt(string $filePath): void
+    {
+        $tmpDirectory = $this->getTemporaryDirectory($filePath);
+        $files = scandir($tmpDirectory);
+        $files = array_filter($files, function ($file) use ($tmpDirectory) {
+            return (str_contains($file, 'img-ocr'));
+        });
+
+        $i = 1;
+        foreach ($files as $item) {
+            $filePath = Path::makeAbsolute($item, $tmpDirectory);
+            shell_exec("tesseract $filePath $tmpDirectory/text-$i --oem 1 --psm 3 -l fra logfile");
+            $i++;
+        }
+        //merge files
+        $ocrFile = $tmpDirectory.DIRECTORY_SEPARATOR.$this::$ocrFilename;
+        shell_exec("cat $tmpDirectory/text-* > $ocrFile");
+    }
+
+    public function getAbsolutePathFromAttachment(\stdClass $attachment): ?string
+    {
+        $guid = $attachment->source_url;
+
+        // Remove https://www.marche.be from the URL
+        $path = str_replace('https://www.marche.be', '', $guid);
+
+        if (str_contains($guid, 'uploads')) {
+            return $this->wpDir.$path;
+        }
+
+        // Extract the first segment (theme name like 'sante', 'sport', etc.)
+        $pathParts = explode('/', trim($path, '/'));
+        $themeName = $pathParts[0] ?? null;
+
+        if ($themeName) {
+            $pathParts[0] = 'blogs.dir/'.Theme::getSiteIdByName($themeName);
+            $path = '/'.implode('/', $pathParts);
+
+            return $this->wpDir.$path;
+        }
+
+        return null;
+    }
+
+    public function fileExists(string $filePath): bool
+    {
+        return is_readable($filePath) && is_file($filePath);
+    }
+
+    /**
+     * @param string $filePath
+     * @return void
+     */
+    public function cleanTmpDirectory(string $filePath): void
+    {
+        $tmpDirectory = $this->getTemporaryDirectory($filePath);
+        $files = scandir($tmpDirectory);
+        // Filter out the '.' and '..' entries
+        $files = array_diff($files, ['.', '..']);
+        foreach ($files as $file) {
+            $filePath = Path::makeAbsolute($file, $tmpDirectory);
+            if (is_file($filePath)) {
+                $this->filesystem->remove($filePath);
+            }
+        }
+    }
+}
