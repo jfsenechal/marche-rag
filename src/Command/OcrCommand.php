4c895361dd6a3825759ef162ec8911e576ac85e0
Senechal jf <jfsenechal@gmail.com>
2025-11-13 09:18:02 +0100

    up

--- src/Command/OcrCommand.php
+++ src/Command/OcrCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -30,6 +31,8 @@ class OcrCommand extends Command
     protected function configure(): void
     {
         $this->addArgument('filePath', InputArgument::OPTIONAL, 'The file path');
+        $this->addOption('extract', 'extract', InputOption::VALUE_NONE, 'Extract text from pdf files');
+        $this->addOption('check', 'check', InputOption::VALUE_NONE, 'Check ocr file exist');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -37,7 +40,30 @@ class OcrCommand extends Command
         $this->io = new SymfonyStyle($input, $output);
 
         $filePath = (string)$input->getArgument('filePath');
+        $check = (bool)$input->getOption('check');
+        $extract = (bool)$input->getOption('extract');
 
+        if ($extract) {
+            $this->extractText();
+            $this->io->success('Finished');
+
+            return Command::SUCCESS;
+        }
+
+        if ($check) {
+            $this->checkOcrFileExist();
+            $this->io->success('Finished');
+
+            return Command::SUCCESS;
+        }
+
+        $this->io->error('Please specify an action. --extract or --check');
+
+        return Command::FAILURE;
+    }
+
+    private function extractText(): void
+    {
         foreach (Theme::getSites() as $siteName) {
             foreach ($this->marcheBeRepository->getAttachments($siteName) as $attachment) {
                 $this->io->title('Extracting pdf: '.$attachment->source_url);
@@ -61,9 +87,22 @@ class OcrCommand extends Command
                 }
             }
         }
+    }
 
-        $this->io->success('Finished');
-
-        return Command::SUCCESS;
+    private function checkOcrFileExist(): void
+    {
+        foreach (Theme::getSites() as $siteName) {
+            foreach ($this->marcheBeRepository->getAttachments($siteName) as $attachment) {
+                $filePath = $this->ocr->resolveAttachmentPath($attachment);
+                if (!$this->ocr->fileExists($filePath)) {
+                    $this->io->writeln("File not found: ".$filePath);
+                } else {
+                    $ocrFilePath = $this->ocr->getOcrOutputPath($filePath);
+                    if (!$this->ocr->fileExists($ocrFilePath)) {
+                        $this->io->writeln("Ocr file not found: ".$filePath);
+                    }
+                }
+            }
+        }
     }
 }
